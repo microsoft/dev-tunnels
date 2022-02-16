5320635d4f157330c143ce8486de28456b30cf1a
DESKTOP-G2KSO1D\fulle <jfullerton@microsoft.com>
2022-02-15 21:37:56 -0800

    add logging to test

--- go/manager_test.go
+++ go/manager_test.go
@@ -2,7 +2,10 @@ package tunnels
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/url"
+	"os"
 	"testing"
 )
 
@@ -16,6 +19,8 @@ func getAccessToken() <-chan string {
 }
 
 func TestTunnelCreate(t *testing.T) {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
 	url, err := url.Parse("https://global.ci.tunnels.dev.api.visualstudio.com/")
 	if err != nil {
 		t.Errorf(err.Error())
@@ -34,5 +39,7 @@ func TestTunnelCreate(t *testing.T) {
 	}
 	if createdTunnel.TunnelID == "" {
 		t.Errorf("tunnel was not successfully created")
+	} else {
+		logger.Println(fmt.Sprintf("Created tunnel with id %s", createdTunnel.TunnelID))
 	}
 }
