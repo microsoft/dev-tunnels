779ecb0fc64491985faeda70a6dedeca0a3c6c8f
Jake Fullerton <jfullerton@microsoft.com>
2022-02-16 15:53:53 -0800

    add tunnel get and delete tests

--- go/manager_test.go
+++ go/manager_test.go
@@ -38,3 +38,89 @@ func TestTunnelCreate(t *testing.T) {
 		logger.Println(fmt.Sprintf("Created tunnel with id %s", createdTunnel.TunnelID))
 	}
 }
+
+func TestTunnelCreateAndGet(t *testing.T) {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	url, err := url.Parse("https://global.ci.tunnels.dev.api.visualstudio.com/")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	managementClient, err := NewManager("Tunnels-Go-SDK", getAccessToken, url, nil)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	tunnel := &Tunnel{}
+	options := &TunnelRequestOptions{}
+	createdTunnel, err := managementClient.CreateTunnel(context.Background(), tunnel, options)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if createdTunnel.TunnelID == "" {
+		t.Errorf("tunnel was not successfully created")
+	} else {
+		logger.Println(fmt.Sprintf("Created tunnel with id %s", createdTunnel.TunnelID))
+	}
+
+	getTunnel, err := managementClient.GetTunnel(context.Background(), createdTunnel, options)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if getTunnel.TunnelID == "" {
+		t.Errorf("tunnel was not successfully found")
+	} else {
+		logger.Println(fmt.Sprintf("Got tunnel with id %s", getTunnel.TunnelID))
+	}
+}
+
+func TestTunnelCreateGetDelete(t *testing.T) {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	url, err := url.Parse("https://global.ci.tunnels.dev.api.visualstudio.com/")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	managementClient, err := NewManager("Tunnels-Go-SDK", getAccessToken, url, nil)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	tunnel := &Tunnel{}
+	options := &TunnelRequestOptions{}
+	createdTunnel, err := managementClient.CreateTunnel(context.Background(), tunnel, options)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if createdTunnel.TunnelID == "" {
+		t.Errorf("tunnel was not successfully created")
+	} else {
+		logger.Println(fmt.Sprintf("Created tunnel with id %s", createdTunnel.TunnelID))
+	}
+
+	getTunnel, err := managementClient.GetTunnel(context.Background(), createdTunnel, options)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if getTunnel.TunnelID == "" {
+		t.Errorf("tunnel was not successfully found")
+	} else {
+		logger.Println(fmt.Sprintf("Got tunnel with id %s", getTunnel.TunnelID))
+	}
+
+	tunnelDeleted, err := managementClient.DeleteTunnel(context.Background(), createdTunnel, options)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if !tunnelDeleted {
+		t.Errorf("tunnel was not successfully deleted")
+	} else {
+		logger.Println(fmt.Sprintf("Deleted tunnel with id %s", getTunnel.TunnelID))
+	}
+}
