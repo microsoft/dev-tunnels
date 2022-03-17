3c06563c2b91b1b679969df8113bcba9a85488ea
Ilya Biryukov <ilbiryuk@microsoft.com>
2022-03-16 17:34:59 -0700

    Add TunnelOptions.isGlobalAccess
    This is needed for Relay service to know whether to redirect,
    or to serve web cross-region.

--- ts/src/contracts/tunnelOptions.ts
+++ ts/src/contracts/tunnelOptions.ts
@@ -1,4 +1,11 @@
 /**
  * Data contract for Tunnel or TunnelPort options.
  */
-export interface TunnelOptions {}
+export interface TunnelOptions {
+    /**
+     * Gets or sets a value indicating whether web-forwarding of this tunnel can run on any cluster
+     * without redirecting to the home cluster.
+     * This is only applicable if the tunnel has a name and web-forwarding uses it.
+     */
+    isGlobalAccess?: boolean;
+}
