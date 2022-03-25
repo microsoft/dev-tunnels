cded583335847a6013d3cedc0adf6d958d8b487c
Jason Ginchereau <jasongin@microsoft.com>
2022-03-25 09:53:27 -1000

    Update generated isGloballyAvailable

--- go/tunnel_options.go
+++ go/tunnel_options.go
@@ -3,4 +3,9 @@
 package tunnels
 
 // Data contract for `Tunnel` or `TunnelPort` options.
-type TunnelOptions struct {}
+type TunnelOptions struct {
+	// Gets or sets a value indicating whether web-forwarding of this tunnel can run on any
+	// cluster (region) without redirecting to the home cluster. This is only applicable if
+	// the tunnel has a name and web-forwarding uses it.
+	IsGloballyAvailable bool `json:"isGloballyAvailable,omitempty"`
+}
--- ts/src/contracts/tunnelOptions.ts
+++ ts/src/contracts/tunnelOptions.ts
@@ -5,9 +5,9 @@
  */
 export interface TunnelOptions {
     /**
-     * Gets or sets a value indicating whether web-forwarding of this tunnel can run on any cluster
-     * without redirecting to the home cluster.
-     * This is only applicable if the tunnel has a name and web-forwarding uses it.
+     * Gets or sets a value indicating whether web-forwarding of this tunnel can run on
+     * any cluster (region) without redirecting to the home cluster. This is only
+     * applicable if the tunnel has a name and web-forwarding uses it.
      */
-    IsGloballyAvailable?: boolean;
+    isGloballyAvailable?: boolean;
 }
