3e480f0fe0fd4fd633716df7f696b9925e68bb82
Jason Ginchereau <jasongin@microsoft.com>
2022-05-13 10:32:48 -1000

    Update headers in new C# files

--- cs/src/Contracts/RateStatus.cs
+++ cs/src/Contracts/RateStatus.cs
@@ -1,5 +1,6 @@
 ﻿// <copyright file="ResourceStatus.cs" company="Microsoft">
 // Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
 // </copyright>
 
 using System.Text.Json.Serialization;
