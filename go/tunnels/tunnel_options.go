33919468663f7ae8f5bf469520b850867f5a6c77
Ilya Biryukov <ilbiryuk@microsoft.com>
2022-10-19 14:18:43 -0700

    Add more properties to control Host and Origin headers

--- go/tunnels/tunnel_options.go
+++ go/tunnels/tunnel_options.go
@@ -9,12 +9,35 @@ type TunnelOptions struct {
 	// Gets or sets a value indicating whether web-forwarding of this tunnel can run on any
 	// cluster (region) without redirecting to the home cluster. This is only applicable if
 	// the tunnel has a name and web-forwarding uses it.
-	IsGloballyAvailable bool `json:"isGloballyAvailable,omitempty"`
+	IsGloballyAvailable     bool `json:"isGloballyAvailable,omitempty"`
 
 	// Gets or sets a value for `Host` header rewriting to use in web-forwarding of this
 	// tunnel or port. By default, with this property null or empty, web-forwarding uses
-	// "localhost" to rewrite the `Host` header. Web-fowarding will use this property instead
-	// if it is not null or empty, . Port-level option, if set, takes precedence over this
-	// option on the tunnel level.
-	HostHeader          string `json:"hostHeader,omitempty"`
+	// "localhost" to rewrite the header. Web-fowarding will use this property instead if it
+	// is not null or empty. Port-level option, if set, takes precedence over this option on
+	// the tunnel level. The option is ignored if IsHostHeaderUnchanged is true.
+	HostHeader              string `json:"hostHeader,omitempty"`
+
+	// Gets or sets a value indicating whether `Host` header is rewritten or the header value
+	// stays intact. By default, if false, web-forwarding rewrites the host header with the
+	// value from HostHeader property or "localhost". If true, the host header will be
+	// whatever the tunnel's web-forwarding host is, e.g. tunnel-name-8080.devtunnels.ms.
+	// Port-level option, if set, takes precedence over this option on the tunnel level.
+	IsHostHeaderUnchanged   bool `json:"isHostHeaderUnchanged,omitempty"`
+
+	// Gets or sets a value for `Origin` header rewriting to use in web-forwarding of this
+	// tunnel or port. By default, with this property null or empty, web-forwarding uses
+	// "http(s)://localhost" to rewrite the header. Web-fowarding will use this property
+	// instead if it is not null or empty. Port-level option, if set, takes precedence over
+	// this option on the tunnel level. The option is ignored if IsOriginHeaderUnchanged is
+	// true.
+	OriginHeader            string `json:"originHeader,omitempty"`
+
+	// Gets or sets a value indicating whether `Origin` header is rewritten or the header
+	// value stays intact. By default, if false, web-forwarding rewrites the origin header
+	// with the value from OriginHeader property or  "http(s)://localhost". If true, the
+	// Origin header will be whatever the tunnel's web-forwarding Origin is, e.g.
+	// https://tunnel-name-8080.devtunnels.ms. Port-level option, if set, takes precedence
+	// over this option on the tunnel level.
+	IsOriginHeaderUnchanged bool `json:"isOriginHeaderUnchanged,omitempty"`
 }
