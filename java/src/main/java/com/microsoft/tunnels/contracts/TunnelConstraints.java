72d413b1c209a9acc896aa02942ea128f1cbabd2
JacobRoberts <jarober@microsoft.com>
2023-06-06 09:37:36 -0700

    Dev/jarober/update cluster id regex (#248)
    * update cluster id regex
    
    * remove localhost
    
    * increment go package version

--- java/src/main/java/com/microsoft/tunnels/contracts/TunnelConstraints.java
+++ java/src/main/java/com/microsoft/tunnels/contracts/TunnelConstraints.java
@@ -104,7 +104,7 @@ public class TunnelConstraints {
      *
      * Cluster IDs are alphanumeric; hyphens are not permitted.
      */
-    public static final String clusterIdPattern = "[a-z][a-z0-9]{2,11}";
+    public static final String clusterIdPattern = "^(([a-z]{3,4}[0-9]{1,3})|asse|aue|brs|euw|use)$";
 
     /**
      * Regular expression that can match or validate tunnel cluster ID strings.
