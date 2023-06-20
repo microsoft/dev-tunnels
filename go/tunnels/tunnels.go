f5c3c59fde6fd03cb47951b9ae59bc6057337de2
Taiyo Sogawa <1176981+taiyosogawa@users.noreply.github.com>
2023-06-20 16:50:12 -0700

    Add user limits to Go SDK (#260)

--- go/tunnels/tunnels.go
+++ go/tunnels/tunnels.go
@@ -10,7 +10,7 @@ import (
 	"github.com/rodaine/table"
 )
 
-const PackageVersion = "0.0.13"
+const PackageVersion = "0.0.14"
 
 func (tunnel *Tunnel) requestObject() (*Tunnel, error) {
 	convertedTunnel := &Tunnel{
