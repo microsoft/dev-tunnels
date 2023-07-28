b9fde4631647df61d7d05eec910735909ee4c278
Jason Ginchereau <jasongin@microsoft.com>
2023-07-27 14:32:11 -1000

    Fix string literal

--- cs/src/Connections/TunnelHost.cs
+++ cs/src/Connections/TunnelHost.cs
@@ -127,7 +127,7 @@ public abstract class TunnelHost : TunnelConnection, ITunnelHost
             forwarder = await pfs.ForwardFromRemotePortAsync(
                 IPAddress.Loopback,
                 portNumber,
-                'localhost',
+                "localhost",
                 portNumber,
                 cancellation);
         }
