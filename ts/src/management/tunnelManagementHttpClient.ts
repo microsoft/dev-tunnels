adb59e670bc0c64d4715d66068dd0f26aeacc9d8
Utsa Santhosh <usasanthosh@microsoft.com>
2024-02-22 15:52:12 -0800

    adding space

--- ts/src/management/tunnelManagementHttpClient.ts
+++ ts/src/management/tunnelManagementHttpClient.ts
@@ -680,7 +680,7 @@ export class TunnelManagementHttpClient implements TunnelManagementClient {
             if (/certificate/i.test((error as AxiosError<any>).message)) {
                 const originalErrorMessage = (error as AxiosError<any>).message;
                 throw new Error("Tunnel service HTTPS certificate is invalid. This may be caused by the use of a " +
-                    "self-signed certificate or a firewall intercepting the connection." + originalErrorMessage + ". ");
+                    "self-signed certificate or a firewall intercepting the connection. " + originalErrorMessage + ". ");
             }
             throw error;
         }
