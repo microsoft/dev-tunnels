106b4e988454dc56468131165d35abb321939e5d
Jason Ginchereau <jasongin@microsoft.com>
2024-09-27 08:49:36 -1000

    Fix host port number in TCP listener factory (#485)

--- ts/src/connections/retryTcpListenerFactory.ts
+++ ts/src/connections/retryTcpListenerFactory.ts
@@ -14,9 +14,10 @@ export class RetryTcpListenerFactory implements TcpListenerFactory {
     public constructor(public readonly localAddress: string) {}
 
     public async createTcpListener(
+        remotePort: number | undefined,
         localIPAddress: string,
         localPort: number,
-        canChangePort: boolean,
+        canChangeLocalPort: boolean,
         cancellation?: CancellationToken,
     ): Promise<Server> {
         // The SSH protocol may specify a local IP address for forwarding, but that is ignored
@@ -33,12 +34,12 @@ export class RetryTcpListenerFactory implements TcpListenerFactory {
             localIPAddress = this.localAddress;
         }
 
-        const maxOffet = 10;
+        const maxOffset = 10;
         const listener = net.createServer();
 
         for (let offset = 0; ; offset++) {
             // After reaching the max offset, pass 0 to pick a random available port.
-            const localPortNumber = offset === maxOffet ? 0 : localPort + offset;
+            const localPortNumber = offset === maxOffset ? 0 : localPort + offset;
 
             try {
                 return await new Promise<Server>((resolve, reject) => {
@@ -48,6 +49,11 @@ export class RetryTcpListenerFactory implements TcpListenerFactory {
                         ipv6Only: net.isIPv6(localIPAddress),
                     });
                     listener.on('listening', () => {
+                        if (remotePort) {
+                            const { address, port } = <net.AddressInfo>listener.address();
+                            console.log(`Forwarding from ${address}:${port} to host port ${remotePort}.`);
+                        }
+
                         resolve(listener);
                     });
                     listener.on('error', (err) => {
@@ -55,9 +61,13 @@ export class RetryTcpListenerFactory implements TcpListenerFactory {
                     });
                 });
             } catch (err) {
-                console.log('Listening on port ' + localPortNumber + ' failed: ' + err);
-                console.log('Incrementing port and trying again');
-                continue;
+                if (offset < maxOffset && canChangeLocalPort) {
+                    console.log('Listening on port ' + localPortNumber + ' failed: ' + err);
+                    console.log('Incrementing port and trying again');
+                    continue;
+                } else {
+                    throw err;
+                }
             }
         }
     }
