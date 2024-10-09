bf96ae5a128041d1a23f81d53a47e9e6c26fdc8d
Jake Fullerton <jfullerton@microsoft.com>
2024-10-09 14:28:48 -0700

    update listener type

--- go/tunnels/tunnels.go
+++ go/tunnels/tunnels.go
@@ -10,7 +10,7 @@ import (
 	"github.com/rodaine/table"
 )
 
-const PackageVersion = "0.1.12"
+const PackageVersion = "0.1.13"
 
 func (tunnel *Tunnel) requestObject() (*Tunnel, error) {
 	convertedTunnel := &Tunnel{
