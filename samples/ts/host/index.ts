47096cbbe8da3789a6c1fde9646ffc2fd58e684b
Ilya Biryukov <ilbiryuk@microsoft.com>
2024-11-06 09:31:02 -0800

    Add sample ts host

---
+++ samples/ts/host/index.ts
@@ -0,0 +1,156 @@
+import {
+  ManagementApiVersions,
+  ProductHeaderValue,
+  TunnelManagementHttpClient,
+  TunnelRequestOptions,
+} from "@microsoft/dev-tunnels-management";
+import {
+  Tunnel,
+  TunnelAccessControlEntryType,
+  TunnelAccessScopes,
+  TunnelEndpoint,
+} from "@microsoft/dev-tunnels-contracts";
+import {
+  TunnelRelayTunnelHost,
+} from "@microsoft/dev-tunnels-connections";
+import * as http from 'http';
+
+const portNumber = 8080;
+const protocol = "http";
+
+function startServer(): Promise<http.Server> {
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!\n');
+  });
+  
+  return new Promise((resolve) => {
+    server.listen(portNumber, () => {
+      console.log(`Server running at ${protocol}://localhost:${portNumber}/`);
+      resolve(server);
+    })
+  });;
+}
+
+async function createTunnelAndConnectHost(tunnelManagementClient: TunnelManagementHttpClient) {
+  const newTunnel: Tunnel = {
+    ports: [{ 
+      portNumber, 
+      protocol,
+    }],
+    accessControl: {
+      entries: [{
+        type: TunnelAccessControlEntryType.Anonymous,
+        subjects: [],
+        scopes: [TunnelAccessScopes.Connect],
+      }],
+    },
+  };
+
+  const tunnelRequestOptions: TunnelRequestOptions = {
+    tokenScopes: [TunnelAccessScopes.Host, TunnelAccessScopes.Connect],
+    includePorts: true,
+  };
+
+  const tunnel = await tunnelManagementClient.createTunnel(
+    newTunnel,
+    tunnelRequestOptions
+  );
+
+  const host = new TunnelRelayTunnelHost(tunnelManagementClient);
+  host.trace = (level, eventId, msg, err) => {
+    console.log(`host: ${msg}`);
+  };
+
+  await host.connect(tunnel!);
+
+  console.log('Tunnel host connected:');
+  const clientAccessToken = tunnel.accessTokens?.[TunnelAccessScopes.Connect];
+  if (clientAccessToken) {
+    console.log(`    Client access token: ${clientAccessToken}`);
+  }
+
+  tunnel.ports?.forEach(({portNumber, portForwardingUris}) => {
+    console.log(`    Tunnel port: ${portNumber}`);
+    if (portForwardingUris) {
+      portForwardingUris.forEach((uri) => console.log(`        Port forwarding URI: ${uri}`));
+    } else {
+      newTunnel.endpoints?.forEach((endpoint) => {
+        console.log(`        Port forwarding URI: ${TunnelEndpoint.getPortUri(endpoint, portNumber)}`);
+      });
+    }
+  });
+
+  return { tunnel, host };
+}
+
+function readAnyKey() {
+  return new Promise<void>((resolve) => {
+    // Debugger console may not support raw mode. Check if it's available.
+    if (typeof process.stdin.setRawMode === "function") {
+      process.stdin.setRawMode(true);
+    }
+
+    process.stdin.resume();
+    process.stdin.once("data", () => {
+      if (typeof process.stdin.setRawMode === "function") {
+        process.stdin.setRawMode(false);
+      }
+
+      resolve();
+      });  
+  });
+}
+
+const aadToken = process.env.AAD_TOKEN;
+if (!aadToken) {
+  console.error("AAD_TOKEN environment variable is required. You can get your AAD token by running 'devtunnels login --verbose' and copying the token from the output.");
+  process.exit(1);
+}
+
+function createTunnelManagementClient() {
+  const userAgent: ProductHeaderValue = {
+    name: "devtunnels-sample-host",
+    version: "1.0.0",
+  };
+
+  const tunnelManagementClient = new TunnelManagementHttpClient(
+    userAgent,
+    ManagementApiVersions.Version20230927preview,
+    () => Promise.resolve(`Bearer ${aadToken}`)
+  );
+
+  tunnelManagementClient.trace = (msg) => {
+    console.log(`tunnel: ${msg}`);
+  }
+
+  return tunnelManagementClient;
+}
+
+async function main() {
+  try {
+    const server = await startServer();
+
+    const tunnelManagementClient = createTunnelManagementClient();
+  
+    console.log("\nCreating tunnel and starting host...");
+    const { tunnel, host } = await createTunnelAndConnectHost(tunnelManagementClient);
+    console.log("\nPress any key to exit...\n");
+    await readAnyKey();
+
+    console.log("\nStopping tunnel host..."); 
+    host.dispose();
+    server.close();
+
+    console.log("\nDeleting tunnel...");
+    await tunnelManagementClient.deleteTunnel(tunnel);
+
+  } catch (err) {
+    console.error(err);
+  }
+
+  process.exit();
+}
+
+main();
