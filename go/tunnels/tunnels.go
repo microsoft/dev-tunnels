67e18f3f03e0834b861753eba0bf22e06ab9c63c
Arun Gopinathan <agopinathan@microsoft.com>
2026-03-18 11:34:25 -0500

    update version requirements for go and ts

--- go/tunnels/tunnels.go
+++ go/tunnels/tunnels.go
@@ -10,7 +10,7 @@ import (
 	"github.com/rodaine/table"
 )
 
-const PackageVersion = "0.1.20"
+const PackageVersion = "0.1.21"
 
 func (tunnel *Tunnel) requestObject() (*Tunnel, error) {
 	convertedTunnel := &Tunnel{
