057a6aad0802162472756a3205118497c083c4e1
Arun Gopinathan <agopinathan@microsoft.com>
2026-03-25 16:28:43 -0500

    bump go package version

--- go/tunnels/tunnels.go
+++ go/tunnels/tunnels.go
@@ -10,7 +10,7 @@ import (
 	"github.com/rodaine/table"
 )
 
-const PackageVersion = "0.1.21"
+const PackageVersion = "0.1.22"
 
 func (tunnel *Tunnel) requestObject() (*Tunnel, error) {
 	convertedTunnel := &Tunnel{
