75c9920bee92a20bbc751d18897afe92d4f3a637
juanpacostaaa <juanpacostaaa@github.com>
2026-04-22 10:12:39 -0700

    Updated GitHub app ids for new apps and created new config for running locally, for each language

--- go/tunnels/tunnel_service_properties.go
+++ go/tunnels/tunnel_service_properties.go
@@ -82,6 +82,18 @@ var prodGitHubAppClientID = "Iv1.e7b89e013f801f03"
 
 // GitHub App Client ID for 'Visual Studio Tunnel Service - Test'
 //
-// Used by client apps that authenticate tunnel users with GitHub, in the PPE and DEV
-// service environments.
-var nonProdGitHubAppClientID = "Iv1.b231c327f1eaa229"
+// Used by client apps that authenticate tunnel users with GitHub, in the PPE
+// service environment.
+var ppeGitHubAppClientID = "Iv1.b231c327f1eaa229"
+
+// GitHub App Client ID for 'Dev Tunnels Service - Dev'
+//
+// Used by client apps that authenticate tunnel users with GitHub, in the DEV
+// service environment.
+var devGitHubAppClientID = "Iv23ctTiak9wLCiTcEbr"
+
+// GitHub App Client ID for 'Dev Tunnels Service - Local'
+//
+// Used by client apps that authenticate tunnel users with GitHub, when running
+// the service locally.
+var localGitHubAppClientID = "Iv23cttBYzKThF88PiPR"
